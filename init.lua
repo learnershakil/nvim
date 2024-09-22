bbc51d56fb775322b692c0d82c8cfdfb29e8e5ed
learnershakil <learnershakil@gmail.com>
2024-09-22 17:13:01 +0530

    setup copilot

--- init.lua
+++ init.lua
@@ -1,2 +1,43 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- To see dotfile and gitignored files, you can use the following setup:
+require("neo-tree").setup({
+    filesystem = {
+      filtered_items = {
+        visible = true, -- Keep dotfiles visible
+        hide_dotfiles = false, -- Show dotfiles (like .env)
+        hide_gitignored = false, -- Optionally show gitignored files
+        hide_by_name = { -- Hide specific directories by name
+          ".git",
+          "node_modules"
+        }
+      }
+    }
+  })
+  
+  -- To use powershell instead of cmd, you can use the following setup:
+  vim.opt.shell = "powershell"
+  vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
+  
+-- TO apply autosave after a second of leaving insert mode or when text is changed, you can use the following setup:
+local autosave_timer = nil
+
+function AutoSave()
+  if autosave_timer then
+    vim.fn.timer_stop(autosave_timer) -- Stop any existing timer
+  end
+  autosave_timer = vim.fn.timer_start(1000, function() -- Delay in ms (1000ms = 1 second)
+    if vim.bo.modified then
+      vim.cmd("silent write") -- Save the file silently
+    end
+  end)
+end
+
+-- Trigger autosave after leaving insert mode or when text is changed
+vim.api.nvim_create_autocmd({"InsertLeave", "TextChanged"}, {
+  callback = AutoSave
+})
+  
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,19 +16,23 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
-    -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import any extra plugins here
-    {import = "lazyvim.plugins.extras.lang.typescript"},
-    -- {import = "lazyvim.plugins.extras.lang.java"},
-    {import = "lazyvim.plugins.extras.lang.python     "},
-    {import = "lazyvim.plugins.extras.lang.php"},
-    -- {import = "lazyvim.plugins.extras.lang.rust"},
-    {import = "lazyvim.plugins.extras.lang.json"},
-    {import = "lazyvim.plugins.extras.ui.mini-animate"},
-    -- import/override with your plugins
-    { import = "plugins" },
-  },
+       -- add LazyVim and import its plugins
+       { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+       -- import any extra plugins here
+       {import = "lazyvim.plugins.extras.lang.typescript"},
+       {import = "lazyvim.plugins.extras.lang.java"},
+       {import = "lazyvim.plugins.extras.lang.python"},
+       {import = "lazyvim.plugins.extras.lang.php"},
+       {import = "lazyvim.plugins.extras.lang.rust"},
+       {import = "lazyvim.plugins.extras.lang.json"},
+       {import = "lazyvim.plugins.extras.ui.mini-animate"},
+       { import = "lazyvim.plugins.extras.linting.eslint" },
+       { import = "lazyvim.plugins.extras.formatting.prettier" },
+       { import = "lazyvim.plugins.extras.lang.tailwind" },
+       { import = "lazyvim.plugins.extras.coding.copilot" },
+       { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+       { import = "plugins" },
+     },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
